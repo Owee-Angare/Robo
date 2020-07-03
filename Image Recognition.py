b63310f36fb3772af8b124e093d20f20d9450c37
Owee-Angare <64698272+Owee-Angare@users.noreply.github.com>
2020-07-03 23:47:05 +0800

    Add files via upload

---
+++ Image Recognition.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('Albert_Einstein.jpg')
+image=cv2.imread('AlbertEinstein_changed.jpg')
+difference=cv2.subtract(img,image)
+result = not np.any(difference) 
+if result is True:
+    print('This is the same image')
+else:
+    print('This is not the same image')
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
